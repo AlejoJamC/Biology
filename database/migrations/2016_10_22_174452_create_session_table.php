a1d38a835518a0009be33d5eeb38b844837f2649
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-10-22 12:58:40 -0500

    database migration modified

--- database/migrations/2016_10_22_174452_create_session_table.php
+++ database/migrations/2016_10_22_174452_create_session_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateSessionTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('usuarios', function (Blueprint $table) {
+        Schema::create('sesion', function (Blueprint $table){
             $table->increments('id');
-            $table->string('nombre');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->integer('id_estudiante');
+            $table->dateTime('inicio_sesion');
+            $table->dateTime('final_sesion');
         });
     }
 
@@ -30,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('usuarios');
+        Schema::dropIfExists('sesion');
     }
 }
