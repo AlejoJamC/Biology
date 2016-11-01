c26074d727c8ae094736d60ad30da6d4155620af
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-11-01 12:35:56 -0500

    admin layout

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+@include('partials.admin.head')
+<body data-theme="theme-3" class="sidebar-menu-added">
+
+<!-- HEADER -->
+@include('partials.admin.header')
+
+<!-- LOADER -->
+<div id="loader-wrapper">
+    <img src="{{ URL::asset('img/theme-3/logo.png') }}" alt=""/>
+    <span></span>
+</div>
+
+<div id="content-wrapper">
+
+    <div class="blocks-container">
+
+        @yield('content')
+
+    </div>
+
+</div>
+
+@include('partials.admin.scripts')
+
+</body>
+</html>
