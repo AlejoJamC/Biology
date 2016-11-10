d23762913831f0fa411f1f8a5c7b3b6050832815
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2016-11-10 18:03:14 -0500

    login / register code

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,28 +13,21 @@
             <div class="row-view">
                 <div class="cell-view">
                     <div class="teaser-logo">
-                        <a href="index.html" class="logo"><img src="{{ URL::asset('img/theme-3/logo-big.png') }}" alt="" /></a>
+                        <a href="index.html" class="logo"><img src="{{ URL::asset('img/logobiova_168_min.png') }}" alt="" /></a>
                     </div>
                 </div>
             </div>
             <div class="row-view">
                 <div class="cell-view">
                     <div class="content-404">
-                        <div class="title">404</div>
+                        <div class="title" style="font-size: 70px;">Iniciar sesión</div>
                         <div class="login-page">
-                            <div class="form">
-                                <form class="register-form">
-                                    <input type="text" placeholder="name"/>
-                                    <input type="password" placeholder="password"/>
-                                    <input type="text" placeholder="email address"/>
-                                    <button>create</button>
-                                    <p class="message">Already registered? <a href="#">Sign In</a></p>
-                                </form>
+                            <div class="form" >
                                 <form class="login-form">
-                                    <input type="text" placeholder="username"/>
-                                    <input type="password" placeholder="password"/>
-                                    <button>login</button>
-                                    <p class="message">Not registered? <a href="#">Create an account</a></p>
+                                    <input type="text" placeholder="email"/>
+                                    <input type="password" placeholder="contraseña"/>
+                                    <button>Iniciar sesión</button>
+                                    <p class="message">No esta registrado? <a href="/register">Crear una cuenta</a></p>
                                 </form>
                             </div>
                         </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.auth')
 
+@section('css')
+    <link href="{{ URL::asset('css/login.css') }}" rel="stylesheet" type="text/css" />
+@stop
+
 @section('title', 'Registro | Proyecto de Grado Biologia 5 grado')
 
 @section('content')
@@ -9,19 +13,46 @@
             <div class="row-view">
                 <div class="cell-view">
                     <div class="teaser-logo">
-                        <a href="index.html" class="logo"><img src="{{ URL::asset('img/theme-3/logo-big.png') }}" alt="" /></a>
+                        <a href="index.html" class="logo"><img src="{{ URL::asset('img/logobiova_168_min.png') }}" alt="" /></a>
                     </div>
                 </div>
             </div>
             <div class="row-view">
                 <div class="cell-view">
                     <div class="content-404">
-                        <div class="title">Nuevo 404</div>
-                        <div class="description">The page you are looking for is missing or currently unavailable. Please use the correct link or visit <a href="index.html">homepage</a>.</div>
+                        <div class="title" style="font-size: 70px;">Crear usuario</div>
+                        <div class="login-page2">
+                            <div class="form">
+                                <form class="login-form">
+                                    <div class="col-md-6">
+                                        <input type="text" placeholder="Nombres"/>
+                                        <input type="text" placeholder="Apellidos"/>
+                                        <input type="password" placeholder="contraseña"/>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <input type="text" placeholder="email"/>
+                                        <select style="width: 100%;">
+                                            <option value="volvo">seleccionar rol ...</option>
+                                            <option value="saab">Estudiante</option>
+                                            <option value="mercedes">Profesor</option>
+                                        </select>
+                                    </div>
+                                    <button>Crear Usuario</button>
+                                    <p class="message">Ya esta registrado? <a href="/login">iniciar sesion</a></p>
+                                </form>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
             @include('partials.auth.footer')
         </div>
     </div>
+    @section('js')
+        <script>
+            $('.message a').click(function(){
+                $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+            });
+        </script>
+    @stop
 @endsection
