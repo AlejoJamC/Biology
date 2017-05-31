1dc824ca78fc6525803e978e66acdd4bd328b6bd
SANLUIS\danilo.cadena <daniloacc1991@gmail.com>
2017-05-31 15:53:14 -0500

    Cerrar Sesion

--- resources/views/partials/admin/sidebar.blade.php
+++ resources/views/partials/admin/sidebar.blade.php
@@ -24,7 +24,13 @@
                     </ul>
                 </li>
                 <li class="submeny">
-                    <a href="/login">Cerrar sesión</a>
+                    <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        Cerrar Sesion
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        {{ csrf_field() }}
+                    </form>
                 </li>
             </ul>
         </nav>
